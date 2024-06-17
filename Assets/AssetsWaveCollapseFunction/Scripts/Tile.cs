b34d6ef1569715b68911d1ce32057cb2a9b054ba
VictorMolez <victor.molez@gmail.com>
2024-06-17 15:51:14 +0200

    WaveCollapse
    La partie génération procédurale utilisant la Wave Function Collapse

---
+++ Assets/AssetsWaveCollapseFunction/Scripts/Tile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile[] left2Neighbours;
+    public Tile[] leftNeighbours;
+    public Tile[] right2Neighbours;
+    public Tile[] rightNeighbours;
+}
